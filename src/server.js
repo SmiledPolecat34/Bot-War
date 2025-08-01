ceeb6bb7c21d65ee31709b3b1e21a85856c33bdc
SmiledPolecat34 <versayo03@gmail.com>
2025-08-01 12:44:41 +0200

    chore(server): update code formatting for consistency

--- src/server.js
+++ src/server.js
@@ -38,5 +38,5 @@ if (require.main === module) {
     const port = process.env.PORT || 3000;
     app.listen(port, () => console.log(`🔌 Controller listening on :${port}`));
 }
-
+// 
 module.exports = app;
